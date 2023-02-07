179bb88a7418a63191e750497a782aeddfea87ed
P@ <patmaynard452@gmail.com>
2023-02-07 16:47:57 -0500

    Added images and more info to the seed for the database
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const randomCity = Math.floor(Math.random() * 1000);
+        const randomPrice = Math.floor(Math.random() * 69) + 420;
         const campground = new Campground ({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[randomCity].city}, ${cities[randomCity].state}`
+            location: `${cities[randomCity].city}, ${cities[randomCity].state}`,
+            price: randomPrice,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Is Camp!"
         });
         await campground.save()
     }
