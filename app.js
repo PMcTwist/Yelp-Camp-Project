b0d1b24168020d25503a6048cf7bb3b3a5de4c3c
P@ <patmaynard452@gmail.com>
2023-02-07 16:48:24 -0500

    Added ejsMate to use ejs injection
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const campground = require("./models/campground");
@@ -24,6 +25,7 @@ db.once("open", () =>{
 // Call and configure instance of express
 const app = express()
 
+app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
