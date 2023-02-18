c09053b508e046c90ef41b77b42cfe153d344fbf
P@ <patmaynard452@gmail.com>
2023-02-18 11:25:10 -0500

    -Added routes for reviews -Added dynamic port allocation
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const catchAsync = require("./utils/catchAsync");
 const expressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
-const { campgroundSchema } = require("./schemas.js");
+const Review = require('./models/review')
+
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
 const ExpressError = require("./utils/ExpressError");
 
 
@@ -43,8 +45,21 @@ const validateCampground = (req, res, next) => {
     if(error) {
         const msg = error.details.map(el => el.message).join(',');
         throw new ExpressError(msg, 400);
+    } 
+    else {
+        next();
     }
-    next();
+};
+
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
 };
 
 
@@ -74,7 +89,7 @@ app.post("/campgrounds", validateCampground, catchAsync(async (req, res) => {
 
 // Show details of camp based on dynamic ID from DB
 app.get("/campgrounds/:id", catchAsync(async (req, res) =>{
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render("campgrounds/show", { campground });
 }));
 
@@ -99,6 +114,22 @@ app.delete("/campgrounds/:id", catchAsync(async (req,res) => {
     res.redirect("/campgrounds");
 }));
 
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
 // Error Handling
 app.all('*', (req, res, next) => {
     next(new expressError('Page Not Found..', 404))
@@ -110,7 +141,11 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
+
+
+const PORT = process.env.PORT || 6969;
+
 // Return a listening string when the app sucessfully goes live
-app.listen(3000, ()=> {
-    console.log("Serving on port 3000")
+app.listen(PORT, ()=> {
+    console.log(`Serving on port ${PORT}`)
 });
