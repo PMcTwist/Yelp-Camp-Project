20d0564d2b55edcd9a51373ea23c6361096fedf2
P@ <patmaynard452@gmail.com>
2023-02-27 16:23:53 -0500

    -Updated routes -Added middleware module -Added user routes module -Updated for authentication
    PM

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const session = require('express-session');
+const flash = require('connect-flash');
 const mongoose = require("mongoose");
 const path = require("path");
 const ejsMate = require("ejs-mate");
-const Joi = require('joi');
-const catchAsync = require("./utils/catchAsync");
 const expressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
-const Campground = require("./models/campground");
-const Review = require('./models/review')
+const passport = require('passport');
+const LocalStrat = require('passport-local');
+const User = require('./models/user');
+
 
 const { campgroundSchema, reviewSchema } = require("./schemas.js");
 const ExpressError = require("./utils/ExpressError");
+
+const UserRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 
 // Setup connection to MongoDB
@@ -19,7 +25,7 @@ mongoose.set("strictQuery", true);
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     // useCreateIndex: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
@@ -28,6 +34,7 @@ db.once("open", () =>{
     console.log("Database connected!")
 });
 
+
 // Call and configure instance of express
 const app = express()
 
@@ -35,100 +42,55 @@ app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded(true));
+app.use(express.urlencoded({ extended: true }));
 // setup _method to be the query string for override
 app.use(methodOverride("_method"));
+// Serve up the static folder
+app.use(express.static(path.join(__dirname, 'public')));
+//Configure session
+const sessionConfig = {
+    secret: 'Thissucks',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(flash());
+
+//Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrat(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if(error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } 
-    else {
-        next();
-    }
-};
+// Flash global handler
+app.use((req, res, next) => {
+    res.locals.returnPage = req.session.returnTo;
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    else {
-        next();
-    }
-};
+
+// Routes
+app.use('/', UserRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 
 // Render Home landing page
 app.get("/", (req, res) =>{
     res.render("home")
 });
-
-// Render different pages for the campgrounds in a RESTful way
-// List an index of camps in teh database
-app.get("/campgrounds", catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds })
-}));
-
-// Add a new campground
-app.get("/campgrounds/new", (req, res) => {
-    res.render("campgrounds/new");
-});
-
-// Post to return new camp form too when compelte
-app.post("/campgrounds", validateCampground, catchAsync(async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-// Show details of camp based on dynamic ID from DB
-app.get("/campgrounds/:id", catchAsync(async (req, res) =>{
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    res.render("campgrounds/show", { campground });
-}));
-
-// Edit camp infos
-// GET to render the edit page for input
-app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    res.render("campgrounds/edit", { campground });
-}));
-
-// POST request for taking the input data from the form
-app.put("/campgrounds/:id", validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-// Delete camp
-app.delete("/campgrounds/:id", catchAsync(async (req,res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds");
-}));
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}));
 
 // Error Handling
 app.all('*', (req, res, next) => {
@@ -140,7 +102,6 @@ app.use((err, req, res, next) => {
     if(!err.message) err.message = "Womp Womp, Something Went Wrong.."
     res.status(statusCode).render('error', { err })
 })
-
 
 
 const PORT = process.env.PORT || 6969;
