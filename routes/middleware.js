0a609519bc5ef8578c749e752ee1904e69ed5b4a
P@ <patmaynard452@gmail.com>
2023-02-28 09:42:50 -0500

    -Added review author middleware
    PM
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- routes/middleware.js
+++ routes/middleware.js
@@ -38,6 +38,18 @@ module.exports.isAuthor = async (req, res, next) => {
     }
 }
 
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)){
+        req.flash('error', "Can't Touch this..");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    else {
+        next();
+    }
+}
+
 module.exports.validateReview = (req, res, next) => {
     const {error} = reviewSchema.validate(req.body);
     if(error) {
